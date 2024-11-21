ffed82679ed49263f4bfb9b37bf45f6e8ee73b86
Ricardo Zamarreno Martins <rizamart@hotmail.com>
2024-11-21 16:50:21 +0000

    con cambios

--- models/staging/sql_server_dbo/stg_sql_server_dbo__order_items.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__order_items.sql
@@ -3,11 +3,10 @@
 with
     src_orders as (select * from {{ source("sql_server_dbo", "orders") }}),
 
-    src_order_items as (select * from {{ source("sql_server_dbo", "order_items") }}),
+    src_order_items as (select product_id, order_id from {{ source("sql_server_dbo", "order_items") }})
 
-    renamed_casted as (
         select
-            order_id,
+            src_order_items.order_id,
             shipping_cost,
             address_id,
             CONVERT_TIMEZONE('UTC', created_at) AS created_at, 
@@ -16,17 +15,13 @@ with
             user_id,
             order_total,
             CONVERT_TIMEZONE('UTC', delivered_at) AS delivered_at,
-        	{{ dbt_utils.surrogate_key([
-				'order_id', 
-				'tracking_id'
-			])
-		    }} as tracking_id,
+            tracking_id,
             case when _fivetran_deleted is null then false end as deleted,
             CONVERT_TIMEZONE('UTC', _fivetran_synced) AS date_load
-        from src_orders
-    )
-
-select *
-from renamed_casted join
+        from src_orders join
     src_order_items on
-    renamed_casted.order_id = src_order_items.order_id
+    src_orders.order_id = src_order_items.order_id
+    
+
+
+
